ff50c7a2c9bc81661e650a1967b3cc0540cf7133
shahnoorhasan <156830237+shahnoorhasan@users.noreply.github.com>
2024-09-28 21:10:12 +0500

    Delete dist directory

--- dist/modules/Admin/Request/Admin.schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.vendorRequestApprovalSchema = void 0;
-const zod_1 = require("zod");
-exports.vendorRequestApprovalSchema = zod_1.z.object({
-    status: zod_1.z.string().toLowerCase(),
-});
