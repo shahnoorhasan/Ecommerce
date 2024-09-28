24031a216b1f7bd0478beeab9d6b821437569b2e
shahnoorhasan <156830237+shahnoorhasan@users.noreply.github.com>
2024-09-28 21:09:50 +0500

    Delete dist directory

--- dist/modules/purchasing/purchasing.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PurchasingRoute = void 0;
-const express_1 = require("express");
-const purchasing_controller_1 = require("./purchasing.controller");
-const purchasingRoutes = (0, express_1.Router)();
-exports.PurchasingRoute = purchasingRoutes;
-purchasingRoutes.post("/purchasing/:id", purchasing_controller_1.createPurchasingHandler);
